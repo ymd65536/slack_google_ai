34e0f86a22ebdb57ed6a5b3400566b8303c99cce
ymd65536 <ymd65536@gmail.com>
2024-03-09 02:34:22 +0900

    modified:

--- image-to-text/app.py
+++ image-to-text/app.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 import os
+import re
 
+from web import web_src
 from slack_mod import share_file
 from img_vertexai import img_text
 from gcs_client import upload
@@ -9,7 +11,6 @@ from slack_bolt import App, Ack
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 
-project_id = os.environ.get("PROJECT_ID", "")
 APP_ENVIRONMENT = os.environ.get("APP_ENVIRONMENT", "")
 SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN", "")
 SLACK_APP_TOKEN = os.environ.get("SLACK_APP_TOKEN", "")
@@ -30,43 +31,61 @@ def count_files_msg(files):
 
 
 def handle_mention(event, say):
+    query = str(re.sub("<@.*>", "", event['text']))
     files = event.get('files', [])
 
+    thread_id = event['ts']
+    if "thread_ts" in event:
+        thread_id = event['thread_ts']
+
+    say('処理中', thread_ts=thread_id)
     if not files:
-        thread_id = event['ts']
-        say('画像が添付されていません！', thread_ts=thread_id)
+        answer = web_src.run_query(query, filter_words=[])
+        say(str(answer['result']), thread_ts=thread_id)
     else:
         early_response = count_files_msg(files=files)
         say(early_response, thread_ts=thread_id)
 
-        thread_id = event['ts']
-
+        bucket_name = os.environ.get("BUCKET_NAME", "")
+        upload_files = []
         for file in files:
             mime_type = file.get('mimetype', None)
-            download_file = file.get('url_private_download', None)
-            upload_file_name = file.get('name', None)
-
-            image = share_file.download_from_slack(download_file, SLACK_BOT_TOKEN)
-
-            if "thread_ts" in event:
-                thread_id = event['thread_ts']
-
-            if image is None or not mime_type == "image/png":
-                say(f"画像リンクから画像が読み取れませんでした。{upload_file_name}", thread_ts=thread_id)
-            else:
+            if mime_type == "image/png":
                 bucket_name = os.environ.get("BUCKET_NAME", "")
-                filename = f"slack_{upload_file_name}_{datetime.now().strftime('%Y%m%d%H%M%S')}.png"
+                download_file = file.get('url_private_download', None)
+                image = share_file.download_from_slack(download_file, SLACK_BOT_TOKEN)
 
-                gcs_file_name = upload.gcs_upload_client(
+                original_file_name = file.get('name', "")
+                user_name = event.get('user', "")
+                original_file_name_part = original_file_name.replace(".png", "")
+                upload_file_name = f"{original_file_name_part}_{user_name}_{datetime.now().strftime('%Y%m%d%H%M%S')}.png"
+
+                gcs_uri = upload.gcs_upload_client(
                     bucket_name=bucket_name,
                     image=image,
-                    filename=filename
+                    filename=upload_file_name
                 )
 
-                res = img_text.img_to_text(gcs_file_name, filename, project_id)
-                say(res, thread_ts=thread_id)
+                upload_files.append(
+                    {
+                        'gcs_uri': gcs_uri,
+                        'filename': original_file_name
+                    }
+                )
 
-    say("チェック終了！", thread_ts=thread_id)
+        thread_id = event['ts']
+        for upload_file in upload_files:
+            if "thread_ts" in event:
+                thread_id = event['thread_ts']
+            print(upload_file['gcs_uri'])
+            result = img_text.img_to_text(
+                query,
+                upload_file['gcs_uri'],
+                upload_file['filename']
+            )
+            say(result, thread_ts=thread_id)
+
+    say('おわり', thread_ts=thread_id)
 
 
 def slack_ack(ack: Ack):
